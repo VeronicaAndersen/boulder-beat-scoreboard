147280742e42aec7df609284606498c214ac2e6f
Veronica Andersen <v.andersen98@gmail.com>
2025-10-30 22:20:56 +0100

    Updated profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,117 +1,106 @@
-import ProblemGrid from "@/components/ProblemGrid";
-import { getClimberById, getCompetitions } from "@/hooks/api";
 import { useAuthStore } from "@/store/auth";
-import { Climber, Competitions } from "@/types";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { SeasonForm } from "@/components/SeasonForm";
+import { SeasonList } from "@/components/SeasonList";
+import { CompetitionForm } from "@/components/CompetitionForm";
+import { CompetitionList } from "@/components/CompetitionList";
+import { Box, Tabs } from "@radix-ui/themes/components/index";
 
 export default function Profile() {
-  const { climberId, setClimberId, setToken } = useAuthStore();
+  const { setClimber, setToken } = useAuthStore();
   const navigate = useNavigate();
 
-  const [climber, setClimberData] = useState<Climber>({
-    id: "",
-    name: "",
-    problemAttempts: [],
-  });
-
-  const [competitionsList, setCompetitions] = useState<Competitions>();
-  const [comp, setComp] = useState<number | null>(null);
-
   useEffect(() => {
-    const storedClimber = localStorage.getItem("climber");
-    if (!storedClimber) {
+    const storedUser = localStorage.getItem("tokens");
+    if (!storedUser) {
       navigate("/");
       return;
     }
 
     try {
-      const parsed = JSON.parse(storedClimber);
-      if (parsed.climberId) {
-        setClimberId(parsed.climberId);
+      const parsed = JSON.parse(storedUser);
+      if (parsed) {
+        setClimber(parsed);
       } else {
-        console.error("Invalid climber data in localStorage:", parsed);
+        console.error("Invalid user data in localStorage:", parsed);
         navigate("/");
       }
     } catch {
-      console.error("Failed to parse climber data from localStorage");
+      console.error("Failed to parse user data from localStorage");
       navigate("/");
     }
-  }, [navigate, setClimberId]);
-
-  useEffect(() => {
-    if (!climberId) return;
-
-    getClimberById(climberId)
-      .then((climber) => {
-        if (climber) {
-          setClimberData(climber);
-        } else {
-          console.error("No climber data found for id:", climberId);
-          navigate("/");
-        }
-      })
-      .catch(() => navigate("/"));
-
-    getCompetitions()
-      .then((competitions) => {
-        if (competitions) {
-          setCompetitions({ competitions: competitions });
-        }
-      })
-      .catch((err) => {
-        console.error("Failed to fetch competitions:", err);
-      });
-  }, [climberId, navigate]);
+  }, [navigate, setClimber]);
 
   const handleLogout = () => {
     setToken(null);
-    setClimberId(null);
-    localStorage.removeItem("climber");
+    localStorage.removeItem("tokens");
     navigate("/");
-  };
-
-  const selectCompetition = (compId: number) => {
-    setComp(compId);
   };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#c6d2b8] px-4">
       <div className="flex flex-col items-center">
         {/* Logo */}
-        <img
-          src="/grepp.svg"
-          alt="Grepp logo"
-          className="w-48 h-28 drop-shadow-md"
-        />
+        <img src="/grepp.svg" alt="Grepp logo" className="w-48 h-28 drop-shadow-md" />
 
-        <div className="flex flex-col items-center justify-center min-h-screen bg-[#c6d2b8] p-4">
-          <div className="w-full max-w-md mb-6">
-            <h2 className="text-2xl font-semibold mb-4">Tävlingar</h2>
-            <ul className="space-y-2">
-              {competitionsList?.competitions.map((comp) => (
-                <li
-                  key={comp.id}
-                  className="p-4 bg-white/90 rounded-2xl shadow hover:bg-white cursor-pointer"
-                  onClick={() => selectCompetition(comp.id)}
-                >
-                  <h3 className="text-xl font-medium">{comp.name}</h3>
-                  <p className="text-sm text-gray-600">Datum: {comp.date}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {/* TODO: selectedGrade should not be hard coded*/}
-          <ProblemGrid competitionId={comp} selectedGrade="Gul" />
+        <Tabs.Root
+          defaultValue="season"
+          className="w-full max-w-2xl mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+        >
+          <Tabs.List color="cyan">
+            <Tabs.Trigger
+              value="season"
+              className="mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+            >
+              Säsong
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="competition"
+              className="mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+            >
+              Tävlingar
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="profil"
+              className="mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+            >
+              Profil
+            </Tabs.Trigger>
+          </Tabs.List>
 
-          <button
-            className="absolute bg-[#505654] hover:bg-[#868f79] rounded px-4 py-2 mt-4 text-white top-4 right-4"
-            onClick={handleLogout}
-          >
-            Logga ut {climber.name}
-          </button>
-        </div>
+          <Box pt="3">
+            <Tabs.Content
+              value="season"
+              className="w-full max-w-2xl mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+            >
+              <SeasonForm />
+              <SeasonList />
+            </Tabs.Content>
+
+            <Tabs.Content
+              value="competition"
+              className="w-full max-w-2xl mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+            >
+              <CompetitionForm />
+              <CompetitionList />
+            </Tabs.Content>
+
+            <Tabs.Content
+              value="profil"
+              className="w-full max-w-2xl mt-6 bg-white/90 backdrop-blur rounded-lg shadow-lg p-4"
+            >
+              <h2 className="text-xl font-semibold mb-4">Profilinformation</h2>
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
       </div>
+      <button
+        className="absolute bg-[#505654] hover:bg-[#868f79] rounded px-4 py-2 mt-4 text-white top-4 right-4"
+        onClick={handleLogout}
+      >
+        Logga ut
+      </button>
     </div>
   );
 }
